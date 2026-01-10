3c7d4864547b9d1a6d1b0294853bd249e4290a87
Zakhar Skorokhodov <21055302+zakharsk@users.noreply.github.com>
2026-01-10 20:34:12 +0100

    Add script to initialize Linuxbrew environment

---
+++ ubuntu_init/register_brew.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -euo pipefail
+
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
